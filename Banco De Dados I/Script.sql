1fea8a3a2472e4f5f5f920246f4ab5d600a85a35
Matheus Henrique <matheus1604.mh@gmail.com>
2023-06-10 19:50:15 -0300

    [refac] modelo visual

--- Banco De Dados I/Script.sql
+++ Banco De Dados I/Script.sql
@@ -6,32 +6,37 @@ SET
 
 CREATE TABLE IF NOT EXISTS nivelFilial(
    nivelFilial varchar(3) NOT NULL,
-   descricao varchar(255) NOT NULL,
+   descricao varchar(256) NOT NULL,
    CONSTRAINT pk_nivel (nivelFilial)
-) CREATE TABLE IF NOT EXISTS metaFilial(
+) 
+CREATE TABLE IF NOT EXISTS metaFilial(
    nivelFilial char(3) NOT NULL,
    vigencia char(6) NOT NULL,
    bonVendedor float NOT NULL,
    bonGerente float NOT NULL,
    CONSTRAINT pk_metaFilial (nivelFilial, vigencia)
-) CREATE TABLE IF NOT EXISTS nivelVendedor(
+) 
+CREATE TABLE IF NOT EXISTS nivelVendedor(
    nivelVendedor char(3) NOT NULL,
    descricao char(256) NOT NULL,
    CONSTRAINT pk_nivelVendedor (nivelVendedor)
-) CREATE TABLE IF NOT EXISTS metaVendedor(
+) 
+CREATE TABLE IF NOT EXISTS metaVendedor(
    nivelVendedor char(3) NOT NULL,
    vigencia char(6) NOT NULL,
    pctgMeta float NOT NULL,
    bonificacao float NOT NULL,
    CONSTRAINT pk_metaVendedor (nivelVendedor, vigencia),
    CONSTRAINT fk_nivelVendedor FOREIGN KEY nivelVendedor(nivelVendedor)
-) CREATE TABLE IF NOT EXISTS localidade(
+) 
+CREATE TABLE IF NOT EXISTS localidade(
    codLocal INTEGER AUTO_INCREMENT,
    cidade varchar(20) NOT NULL,
    estado varchar(2) NOT NULL,
    CONSTRAINT pk_localidade (codLocal),
    CONSTRAINT uk_cidadeEstado UNIQUE (cidade, estado)
-) CREATE TABLE IF NOT EXISTS filial(
+) 
+CREATE TABLE IF NOT EXISTS filial(
    codFilial INTEGER AUTO_INCREMENT,
    -- AUTO INCREMENT faz o código ser incrementado automaticamente
    dtInclusao date NOT NULL,
@@ -47,11 +52,12 @@ CREATE TABLE IF NOT EXISTS nivelFilial(
    CONSTRAINT fk_local FOREIGN KEY (codLocal) REFERENCES localidade(codLocal),
    -- Chave estrangeira para a localidade
    CONSTRAINT fk_nivel FOREIGN KEY (nivelFilial) REFERENCES nivelFilial(nivelFilial) -- chave estrangeira para o nivel de filial
-) CREATE TABLE IF NOT EXISTS usuario(
+) 
+CREATE TABLE IF NOT EXISTS usuario(
    codUsuario INTEGER AUTO_INCREMENT,
    nome char(256) NOT NULL,
    cpf char(11) NOT NULL UNIQUE,
-   username char(256) NOT NULL,
+   login char(256) NOT NULL,
    senha char(256) NOT NULL,
    status_usuario INTEGER NOT NULL,
    dtInclusao date NOT NULL,
@@ -63,7 +69,8 @@ CREATE TABLE IF NOT EXISTS nivelFilial(
    CONSTRAINT uk_cpf UNIQUE (cpf),
    CONSTRAINT fk_filial FOREIGN KEY (filial) REFERENCES filial(codFilial),
    CONSTRAINT fk_nivelVendedor FOREIGN KEY (nivelVendedor) REFERENCES nivelVendedor(nivelVendedor)
-) CREATE TABLE IF NOT EXISTS venda(
+) 
+CREATE TABLE IF NOT EXISTS venda(
    codVendedor INTEGER NOT NULL UNIQUE,
    -- só um vendedor pode fazer a venda né
    dataVenda date NOT NULL,
